9214d124a4feeba08a3aee61eed190c636883280
Brynley Llewellyn-Roux <blynblack@gmail.com>
2025-02-04 11:32:04 +1100

    feat: update CI

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
     nixpkgs-matrix = {
       type = "indirect";
       id = "nixpkgs-matrix";
-      inputs.nixpkgs.url =
-        "github:NixOS/nixpkgs?rev=e69e710edfed397959507bcee120ec8a9c7ff03e";
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -13,17 +11,15 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs-matrix.legacyPackages.${system};
-
         shell = { ci ? false }:
           with pkgs;
-          mkShell {
+          pkgs.mkShell {
             nativeBuildInputs = [ nodejs_20 shellcheck gitAndTools.gh ];
-            NIX_DONT_SET_RPATH = true;
-            NIX_NO_SELF_RPATH = true;
+            PKG_IGNORE_TAG = 1;
             shellHook = ''
               echo "Entering $(npm pkg get name)"
               set -o allexport
-              . ./.env
+              . <(polykey secrets env js-rpc)
               set +o allexport
               set -v
               ${lib.optionalString ci ''
@@ -33,8 +29,11 @@
                 shopt -s inherit_errexit
               ''}
               mkdir --parents "$(pwd)/tmp"
+
               export PATH="$(pwd)/dist/bin:$(npm root)/.bin:$PATH"
+
               npm install --ignore-scripts
+
               set +v
             '';
           };
