3435a113210ef5ce02eea354514e0c351ac9bb12
Brian Botha <brian.botha@matrix.ai>
2025-03-28 10:09:36 +1100

    bench: updating benchmark structure

--- benches/index.ts
+++ benches/index.ts
@@ -1,26 +1,27 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env tsx
 
-import fs from 'fs';
-import path from 'path';
-import url from 'url';
+import fs from 'node:fs';
+import path from 'node:path';
+import url from 'node:url';
 import si from 'systeminformation';
-
-const dirname = url.fileURLToPath(new URL('.', import.meta.url));
+import { benchesPath } from './utils/utils.js';
 
 async function main(): Promise<void> {
-  await fs.promises.mkdir(path.join(dirname, 'results'), { recursive: true });
+  await fs.promises.mkdir(path.join(benchesPath, 'results'), {
+    recursive: true,
+  });
   const resultFilenames = await fs.promises.readdir(
-    path.join(dirname, 'results'),
+    path.join(benchesPath, 'results'),
   );
   const metricsFile = await fs.promises.open(
-    path.join(dirname, 'results', 'metrics.txt'),
+    path.join(benchesPath, 'results', 'metrics.txt'),
     'w',
   );
   let concatenating = false;
   for (const resultFilename of resultFilenames) {
     if (/.+_metrics\.txt$/.test(resultFilename)) {
       const metricsData = await fs.promises.readFile(
-        path.join(dirname, 'results', resultFilename),
+        path.join(benchesPath, 'results', resultFilename),
       );
       if (concatenating) {
         await metricsFile.write('\n');
@@ -36,9 +37,16 @@ async function main(): Promise<void> {
     system: 'model, manufacturer',
   });
   await fs.promises.writeFile(
-    path.join(dirname, 'results', 'system.json'),
+    path.join(benchesPath, 'results', 'system.json'),
     JSON.stringify(systemData, null, 2),
   );
 }
 
-void main();
+if (import.meta.url.startsWith('file:')) {
+  const modulePath = url.fileURLToPath(import.meta.url);
+  if (process.argv[1] === modulePath) {
+    void main();
+  }
+}
+
+export default main;
